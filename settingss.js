53580ecc31cca635e821ab5f2062560a14ce1601
Tawana2 <tawanalunga96@gmail.com>
2025-08-27 06:19:59 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~hzMRQJpb#WjmM_1X0UZAjExxSpXEZq-9Msli0X7ff9Sg76CFwr7g",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263719778322", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
