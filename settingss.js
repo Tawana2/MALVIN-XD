77363c9de673742518b3f065e72584e468de9ea3
Tawana2 <tawanalunga96@gmail.com>
2025-08-30 06:29:02 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~hzMRQJpb#WjmM_1X0UZAjExxSpXEZq-9Msli0X7ff9Sg76CFwr7g",  // add your Session Id here
+  SESSION_ID: "malvin~UikBzT5a#aUlyLU1HWqkF-9Y6u7q2xpfXa_7QZXcC39M6jXUyYRc",  // add your Session Id here
   
   OWNER_NUMBER: "263719778322", // put your phone number here
   
